f11d88f1d8eac23482584249f657d46292fadfbc
Hassan <sychox2006@gmail.com>
2025-09-10 05:14:32 +0500

    Update driveClient.ts

--- utils/driveClient.ts
+++ utils/driveClient.ts
@@ -1,12 +1,9 @@
-import { google } from 'googleapis';
-import dotenv from 'dotenv';
+import { google } from "googleapis";
+import dotenv from "dotenv";
 dotenv.config();
 
-export const getOAuthClient = () => {
-  const oAuth2Client = new google.auth.OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    process.env.REDIRECT_URI
-  );
-  return oAuth2Client;
-};
+export const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
